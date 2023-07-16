826b24e4d6992a757aaa00c2a82d76b2e3c99385
sudeepdm17 <91010456+sudeepdm17@users.noreply.github.com>
2023-07-16 13:13:07 +0530

    Update prg6.c

--- prg6.c
+++ prg6.c
@@ -1,96 +1,48 @@
-#include<stdio.h>
 #include<GL/glut.h>
-
-void init()
+void obj(double tx,double ty,double tz,double sx,double sy,double sz)
 {
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(-4,4,-4,4,-10,10);
-	glMatrixMode(GL_MODELVIEW);
-} // Added closing brace here
-
-void wall()
-{
-	glPushMatrix();
-	glScalef(2,0.05,2);
-	glutSolidCube(2);
-	glPopMatrix();
-
-	glPushMatrix();
-	glTranslatef(-2,2,0);
-	glScalef(0.05,2,2);
-	glutSolidCube(2);
-	glPopMatrix();
-
-	glPushMatrix();
-	glTranslatef(0,2,-2);
-	glScalef(2,2,0.05);
-	glutSolidCube(2);
-	glPopMatrix();
+glRotated(50,0,1,0);
+glRotated(10,-1,0,0);
+glRotated(11.7,0,0,-1);
+glTranslated(tx,ty,tz);
+glScaled(sx,sy,sz);
+glutSolidCube(1);
+glLoadIdentity();
 }
-
-void table()
+void display()
 {
-	glPushMatrix();
-	glTranslatef(0,0.5,0);
-	glScalef(1,0.05,1);
-	glutSolidCube(2);
-	glPopMatrix();
+glViewport(0,0,700,700);
+glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+obj(0,0,0.5,1,1,0.04);
+obj(0,-0.5,0,1,0.04,1); // three walls obj
+obj(-0.5,0,0,0.04,1,1);
 
-	glPushMatrix();
-	glTranslatef(-0.8,0.2,0.8);
-	glScalef(0.1,0.25,0.1);
-	glutSolidCube(2);
-	glPopMatrix();
+obj(0,-0.3,0,0.02,0.2,0.02); // four table legs"
+obj(0,-0.3,-0.4,0.02,0.2,0.02);
+obj(0.4,-0.3,0,0.02,0.2,0.02);
+obj(0.4,-0.3,-0.4,0.02,0.2,0.02);
 
-	glPushMatrix();
-	glTranslatef(0.8,0.2,-0.8);
-	glScalef(0.1,0.25,0.1);
-	glutSolidCube(2);
-	glPopMatrix();
-
-	glPushMatrix();
-	glTranslatef(-0.8,0.2,-0.8);
-	glScalef(0.1,0.25,0.1);
-	glutSolidCube(2);
-	glPopMatrix();
+obj(0.2,-0.18,-0.2,0.6,0.02,0.6); // table top "
+glRotated(50,0,1,0);
+glRotated(10,-1,0,0);
+glRotated(11.7,0,0,-1);
+glTranslated(0.3,-0.1,-0.3);
+glutSolidTeapot(0.09);
+glFlush();
+glLoadIdentity();
 }
-
-void tea()
+int main(int argc, char **agrv)
 {
-	glPushMatrix();
-	glTranslatef(0,1,0);
-	glutSolidTeapot(0.5);
-	glPopMatrix();
-}
-
-void display(void)
-{
-	float amb[]={1,0,0};
-	float pos[]={2,4,1};
-	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-	glMaterialfv(GL_FRONT,GL_AMBIENT,amb);
-	glLightfv(GL_LIGHT0,GL_POSITION,pos);
-	gluLookAt(2,1,2,0,1,0,0,1,0);
-	wall();
-	table();
-	tea();
-	glFlush();
-}
-
-int main(int argc,char ** argv)
-{
-	glutInit(&argc,argv);
-	glutInitDisplayMode(GLUT_SINGLE|GLUT_DEPTH);
-	glutInitWindowSize(600,600);
-	glutCreateWindow("Teapot : C Tathva");
-	init();
-	glutDisplayFunc(display);
-	glEnable(GL_DEPTH_TEST);
-	glShadeModel(GL_SMOOTH);
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
-	glEnable(GL_NORMALIZE);
-	glutMainLoop();
-	return 0;
+float ambient[]={1,1,1,1};
+float light_pos[]={27,80,2,3};
+glutInit(&argc, agrv);
+glutInitWindowSize(700,700);
+glutCreateWindow("scene");
+glutDisplayFunc(display);
+glEnable(GL_LIGHTING);
+glEnable(GL_LIGHT0);
+glMaterialfv(GL_FRONT,GL_AMBIENT,ambient);
+glLightfv(GL_LIGHT0,GL_POSITION,light_pos);
+glEnable(GL_DEPTH_TEST);
+glutMainLoop();
 }
